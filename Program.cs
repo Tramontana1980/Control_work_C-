ab2c74050ed8506331cd3d7b9bb87878585c8633
Alexey Filippchik <fila1980@mail.ru>
2024-02-25 19:06:15 +0300

    Create function CountStringSymbol

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] FillArray(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+
+int CountStringSymbol(string[] stringArray)
+{
+    int n = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+            n++;
+    }
+    return n;
+}
